54bb545e48f8cb4378644f3e5630ed5bcb542425
tsw303005 <tsw303005@gmail.com>
2022-05-15 20:44:11 +0800

    add auth interceptor

--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122
 	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3
-	google.golang.org/grpc v1.46.0
+	google.golang.org/grpc v1.46.2
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0
 	google.golang.org/protobuf v1.28.0
 )
--- pkg/authkit/auth.go
+++ pkg/authkit/auth.go
@@ -1,1 +1,63 @@
 package auth
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+type AuthService struct {
+	JWTManager *JWTManager
+}
+
+func NewAuthService(jwtManager *JWTManager) *AuthService {
+	return &AuthService{
+		JWTManager: jwtManager,
+	}
+}
+
+func (a *AuthService) UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		// check method if needing authentication
+		if _, ok := publicAPI[info.FullMethod]; ok {
+			return handler(ctx, req)
+		}
+
+		// check token if is valid and get user_id
+		err := a.authenticate(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		return handler(ctx, req)
+	}
+}
+
+func (a *AuthService) authenticate(ctx context.Context) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return MetaDataNotProvided
+	}
+
+	values := md["authorization"]
+	if len(values) == 0 {
+		return TokenNotProvided
+	}
+
+	accessToken := values[0]
+	payload, err := a.JWTManager.Verify(accessToken)
+	if err != nil {
+		return TokenInvalid
+	}
+
+	// append user id to context
+	md.Append("user_id", payload.Id)
+
+	return nil
+}
